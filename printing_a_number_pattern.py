ce2f4a3fd481185c72d0823db5765d61267d93c9
unknown <emgcalangian@gmail.com>
2024-01-22 17:36:25 +0800

    main code done

--- printing_a_number_pattern.py
+++ printing_a_number_pattern.py
@@ -4,5 +4,8 @@
 
 # ask user how many layers of the pyramid do they want
 layers = int(input("How many layers do you want your pyramid to be? "))
-print(layers)
+
 # use for loop to create pattern
+for outer in range(layers + 1):
+    for inner in range(outer):
+        print(outer)
